16124807ed7743ef36ad8e8d93f01182bee48a3b
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-13 21:43:48 +0700

    upddate

--- pendaftaran/dashboard_pendaftaran.php
+++ pendaftaran/dashboard_pendaftaran.php
@@ -73,7 +73,7 @@ $conn->close();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="../assets/css/dashboard_ppdb.css">
+  <link rel="stylesheet" href="../assets/css/pendaftaran_dashboard_styles.css">
   <!-- Chart.js -->
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
