ab15479b20576df1761d859fdd667a457ef7279c
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-26 21:59:15 +0700

    update

--- pendaftaran/form_pendaftaran.php
+++ pendaftaran/form_pendaftaran.php
@@ -28,12 +28,10 @@ function display_errors() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Pendaftaran Siswa Baru</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="../assets/css/pendaftaran_dashboard_styles.css" />
-    <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css" />
-    <link href="../assets/css/form_pendaftaran_styles.css" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
+    <link rel="stylesheet" href="../assets/css/form_pendaftaran_styles.css">
 </head>
 <body style="background: linear-gradient(110deg, #f0f4ff 0%, #f5f8ff 30%, #e8eeff 100%);">
 
