e629d10de81c99db599b82e5d5e760f0887690fd
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-26 21:56:18 +0700

    update

--- pendaftaran/form_pendaftaran.php
+++ pendaftaran/form_pendaftaran.php
@@ -30,8 +30,10 @@ function display_errors() {
     <title>Form Pendaftaran Siswa Baru</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
-    <link rel="stylesheet" href="../assets/css/form_pendaftaran_styles.css">
+    <link href="../assets/css/pendaftaran_dashboard_styles.css" rel="stylesheet">
+    <link href="../assets/css/sidebar_pendaftaran_styles.css" rel="stylesheet">
+    <link href="../assets/css/form_pendaftaran_styles.css" rel="stylesheet">
+
 </head>
 <body style="background: linear-gradient(110deg, #f0f4ff 0%, #f5f8ff 30%, #e8eeff 100%);">
 
