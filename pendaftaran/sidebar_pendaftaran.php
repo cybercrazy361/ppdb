cef0fb09f5d4943f8f29cf28ad21b657ec23e3c9
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-26 23:14:42 +0700

    update

--- pendaftaran/sidebar_pendaftaran.php
+++ pendaftaran/sidebar_pendaftaran.php
@@ -9,7 +9,7 @@
 
   <a href="form_pendaftaran.php"
      class="nav-link <?= ($active=='form'?'active':'') ?>">
-    <i class="fas fa-user-plus"></i><span> Input</span>
+    <i class="fas fa-user-plus"></i><span> Input Progress</span>
   </a>
 
   <a href="daftar_siswa.php"
