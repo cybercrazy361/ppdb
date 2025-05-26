e055a3b9fe728ffd9b430bdc4f5f42a50c0eadee
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-26 22:41:56 +0700

    update

--- pendaftaran/daftar_siswa.php
+++ pendaftaran/daftar_siswa.php
@@ -123,7 +123,6 @@ function getStatusPembayaranLabel($status) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
-  <link rel="stylesheet" href="../assets/css/pendaftaran_dashboard_styles.css">
   <link rel="stylesheet" href="../assets/css/daftar_siswa_styles.css">
 </head>
 <body>
