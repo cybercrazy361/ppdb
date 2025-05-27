137b5194da54faa94a36cf9724916173b99b0984
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-27 22:18:21 +0700

    upddate

--- pendaftaran/input_progres_pendaftaran.php
+++ pendaftaran/input_progres_pendaftaran.php
@@ -12,9 +12,9 @@ if (empty($_SESSION['csrf_token'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendaftaran Awal - Yayasan Pendidikan Dharma Karya</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-    <link rel="stylesheet" href="../assets/css/input_progres_pendaftaran_styles.css">
-    <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
+  <link rel="stylesheet" href="../assets/css/input_progres_pendaftaran_styles.css">
 </head>
 <body>
     <?php $active = 'inputpendaftaran'; ?>
