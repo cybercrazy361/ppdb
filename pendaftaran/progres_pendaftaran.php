8f1b1b1ee638f48adfa6e1f2301e787faa307091
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-27 22:30:03 +0700

    upddate

--- pendaftaran/progres_pendaftaran.php
+++ pendaftaran/progres_pendaftaran.php
@@ -64,7 +64,7 @@ function sudah_terkirim($conn, $nama, $tanggal_daftar) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Progres Pendaftaran - PPDB</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
   <link rel="stylesheet" href="../assets/css/progres_pendaftaran_styles.css">
