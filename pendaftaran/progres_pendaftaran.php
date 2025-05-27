bac76cef33be1683a5aadd02dae4fa63215a39a1
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-27 22:16:41 +0700

    upddate

--- pendaftaran/progres_pendaftaran.php
+++ pendaftaran/progres_pendaftaran.php
@@ -64,12 +64,10 @@ function sudah_terkirim($conn, $nama, $tanggal_daftar) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Progres Pendaftaran - PPDB</title>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
-  <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
-  <link href="../assets/css/progres_pendaftaran_styles.css" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/progres_pendaftaran_styles.css">
 </head>
 <body>
     <?php $active = 'progrespendaftaran'; ?>
