cf5732cb57081911ed6b409cf5d049e214125efe
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-29 20:29:50 +0700

    update

--- callcenter/daftar_calon_siswa.php
+++ callcenter/daftar_calon_siswa.php
@@ -221,7 +221,6 @@ function sudah_terkirim($conn, $nama, $tanggal_daftar) {
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-<script src="../assets/js/sidebar_pendaftaran.js"></script>
 <script>
 $(function(){
   // DataTable dan event badge filter tetap
