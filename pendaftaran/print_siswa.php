8aed0ba940a69fb8bf7589c69733a9ee2004c8c5
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-30 00:36:35 +0700

    update

--- pendaftaran/print_siswa.php
+++ pendaftaran/print_siswa.php
@@ -158,49 +158,49 @@ $no_invoice = $row['no_invoice'] ?? '';
     <i class="fas fa-print"></i> Cetak
   </button>
   <div class="container">
-<div class="kop-surat-rel">
-  <img src="../assets/images/logo_trans.png" alt="Logo" class="kop-logo-abs" />
-  <div class="kop-info-center">
-    <div class="kop-title1">YAYASAN PENDIDIKAN DHARMA KARYA</div>
-    <div class="kop-title2">SMA/SMK DHARMA KARYA</div>
-    <div class="kop-akreditasi"><b>Terakreditasi “A”</b></div>
-    <div class="kop-alamat">Jalan Melawai XII No.2 Kav. 207A Kebayoran Baru Jakarta Selatan</div>
-    <div class="kop-alamat">Telp. 021-7398578 / 7250224</div>
-  </div>
-</div>
-<div class="kop-garis"></div>
-
-    <div class="header">
-      <div class="header-content">
-        <?php if ($status_pembayaran === 'Lunas' || $status_pembayaran === 'Angsuran'): ?>
-          <div class="sub-title"><b>BUKTI PENDAFTARAN MURID BARU</b></div>
-        <?php else: ?>
-          <div class="sub-title"><b>BUKTI PENDAFTARAN CALON MURID BARU</b></div>
-        <?php endif; ?>
-        <div class="tahun-ajaran"><b>SISTEM PENERIMAAN MURID BARU (SPMB)</b></div>
-        <div class="tahun-ajaran"><b>SMA DHARMA KARYA JAKARTA</b></div>
-        <div class="tahun-ajaran" style="font-size: 13px;"><b>TAHUN AJARAN 2025/2026</b></div>
+    <!-- Kop Surat -->
+    <div class="kop-surat">
+      <div class="kop-logo">
+        <img src="../assets/images/logo_trans.png" alt="Logo Sekolah">
+      </div>
+      <div class="kop-center">
+        <div class="kop-title-1">YAYASAN PENDIDIKAN DHARMA KARYA</div>
+        <div class="kop-title-2">SMA/SMK DHARMA KARYA</div>
+        <div class="kop-akreditasi">Terakreditasi <b>“A”</b></div>
+        <div class="kop-alamat">Jl. Melawai XII No.2 Kav. 207A Kebayoran Baru, Jakarta Selatan</div>
+        <div class="kop-telp">Telp. 021-7398578 / 7250224</div>
       </div>
     </div>
+    <div class="kop-garis"></div>
 
-    <div class="no-reg-row">
-      <div class="no-reg-label"><b>No. Registrasi Pendaftaran</b></div>
-      <div class="no-reg-sep">:</div>
-      <div class="no-reg-val"><b><i><?= safe($row['no_formulir']) ?></i></b></div>
+    <!-- Header -->
+    <div class="header">
+      <?php if ($status_pembayaran === 'Lunas' || $status_pembayaran === 'Angsuran'): ?>
+        <div class="sub-title">BUKTI PENDAFTARAN MURID BARU</div>
+      <?php else: ?>
+        <div class="sub-title">BUKTI PENDAFTARAN CALON MURID BARU</div>
+      <?php endif; ?>
+      <div class="tahun-ajaran">SISTEM PENERIMAAN MURID BARU (SPMB)</div>
+      <div class="tahun-ajaran">SMA DHARMA KARYA JAKARTA</div>
+      <div class="tahun-ajaran tahun-ajaran-thin">TAHUN AJARAN 2025/2026</div>
+    </div>
+
+    <!-- Data Registrasi -->
+    <div class="reg-row">
+      <span class="reg-label">No. Registrasi Pendaftaran</span>
+      <span class="reg-sep">:</span>
+      <span class="reg-val"><?= safe($row['no_formulir']) ?></span>
     </div>
     <?php if ($status_pembayaran !== 'Belum Bayar' && !empty($no_invoice)): ?>
-      <div class="no-reg-row">
-        <div class="no-reg-label"><b>No. Formulir Pendaftaran</b></div>
-        <div class="no-reg-sep">:</div>
-        <div class="no-reg-val"><b><i><?= safe($no_invoice) ?></i></b></div>
-      </div>
+    <div class="reg-row">
+      <span class="reg-label">No. Formulir Pendaftaran</span>
+      <span class="reg-sep">:</span>
+      <span class="reg-val"><?= safe($no_invoice) ?></span>
+    </div>
     <?php endif; ?>
 
+    <!-- Data Siswa -->
     <table class="data-table">
-      <colgroup>
-        <col style="width:40%">
-        <col style="width:60%">
-      </colgroup>
       <caption>DATA CALON PESERTA DIDIK BARU</caption>
       <tr><th>Tanggal Pendaftaran</th><td><?= tanggal_id($row['tanggal_pendaftaran']) ?></td></tr>
       <tr><th>Nama Calon Peserta Didik</th><td><?= safe($row['nama']) ?></td></tr>
@@ -211,95 +211,87 @@ $no_invoice = $row['no_invoice'] ?? '';
       <tr><th>No. HP Orang Tua/Wali</th><td><?= safe($row['no_hp_ortu']) ?></td></tr>
       <tr><th>Pilihan Sekolah/Jurusan</th><td><?= safe($row['unit']) ?></td></tr>
     </table>
-    <table class="tagihan-table" style="margin-top:25px;">
+
+    <!-- Tagihan -->
+    <table class="tagihan-table">
       <tr>
-        <th colspan="2" style="background:#e3eaf7;font-size:15.5px;text-align:center">
-          <i class="fas fa-coins"></i> Keterangan Pembayaran
-        </th>
+        <th colspan="2"><i class="fas fa-coins"></i> Keterangan Pembayaran</th>
       </tr>
       <?php if(count($tagihan)): foreach($tagihan as $tg): ?>
       <tr>
         <td><?= safe($tg['jenis']) ?></td>
-        <td style="text-align:right;font-weight:600">
-          Rp <?= number_format($tg['nominal'], 0, ',', '.') ?>
-        </td>
+        <td class="text-right"><b>Rp <?= number_format($tg['nominal'], 0, ',', '.') ?></b></td>
       </tr>
       <?php endforeach; else: ?>
       <tr>
-        <td colspan="2" style="text-align:center;color:#bb2222;">Belum ada tagihan yang diverifikasi.</td>
+        <td colspan="2" class="text-center muted">Belum ada tagihan yang diverifikasi.</td>
       </tr>
       <?php endif; ?>
     </table>
+
+    <!-- Riwayat Pembayaran -->
     <?php if ($status_pembayaran !== 'Belum Bayar' && count($pembayaran_terakhir)): ?>
-      <div style="margin:18px 0 4px 0;font-size:15.2px;font-weight:500;">Riwayat Pembayaran:</div>
-      <table class="tagihan-table riwayat-bayar" style="margin-bottom:18px;">
-        <colgroup>
-          <col style="width:18%">
-          <col style="width:18%">
-          <col style="width:18%">
-          <col style="width:14%">
-          <col style="width:10%">
-          <col style="width:22%">
-        </colgroup>
-        <tr>
-          <th>Jenis</th>
-          <th>Nominal</th>
-          <th>Cashback</th>
-          <th>Status</th>
-          <th>Bulan</th>
-          <th>Tanggal</th>
-        </tr>
-        <?php foreach($pembayaran_terakhir as $b): ?>
-        <tr>
-          <td><?= safe($b['jenis']) ?></td>
-          <td style="text-align:right;">Rp <?= number_format($b['jumlah'],0,',','.') ?></td>
-          <td style="text-align:right;">
-            <?= ($b['cashback'] ?? 0) > 0 ? 'Rp ' . number_format($b['cashback'],0,',','.') : '-' ?>
-          </td>
-          <td><?= safe($b['status_pembayaran']) ?></td>
-          <td><?= $b['bulan'] ? safe($b['bulan']) : '-' ?></td>
-          <td class="tgl-lebar"><?= tanggal_id($b['tanggal_pembayaran']) ?></td>
-        </tr>
-        <?php endforeach; ?>
-      </table>
+    <div class="riwayat-title">Riwayat Pembayaran:</div>
+    <table class="tagihan-table riwayat-bayar">
+      <tr>
+        <th>Jenis</th>
+        <th>Nominal</th>
+        <th>Cashback</th>
+        <th>Status</th>
+        <th>Bulan</th>
+        <th>Tanggal</th>
+      </tr>
+      <?php foreach($pembayaran_terakhir as $b): ?>
+      <tr>
+        <td><?= safe($b['jenis']) ?></td>
+        <td class="text-right">Rp <?= number_format($b['jumlah'],0,',','.') ?></td>
+        <td class="text-right"><?= ($b['cashback'] ?? 0) > 0 ? 'Rp ' . number_format($b['cashback'],0,',','.') : '-' ?></td>
+        <td><?= safe($b['status_pembayaran']) ?></td>
+        <td><?= $b['bulan'] ? safe($b['bulan']) : '-' ?></td>
+        <td><?= tanggal_id($b['tanggal_pembayaran']) ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </table>
     <?php endif; ?>
 
-        <div class="status-row">
-          Status Pembayaran: <?= getStatusBadge($status_pembayaran) ?>
-        </div>
-
-    <div class="row-btm">
-      <div class="info-contact">
-        Informasi lebih lanjut hubungi:<br>
-        Hotline SMA : <b>081511519271</b> (Bu Puji)
-      </div>
+    <!-- Status pembayaran -->
+    <div class="status-row">
+      Status Pembayaran: <?= getStatusBadge($status_pembayaran) ?>
     </div>
+
+    <!-- Kontak -->
+    <div class="info-contact">
+      Informasi lebih lanjut hubungi: <br>
+      Hotline SMA: <b>081511519271</b> (Bu Puji)
+    </div>
+
+    <!-- Catatan -->
     <div class="note <?= $note_class ?>">
       <?php if ($status_pembayaran === 'Belum Bayar'): ?>
         <b>Catatan:</b><br>
-        1.	Apabila telah menyelesaikan administrasi, serahkan kembali form pendaftaran ini ke bagian pendaftaran untuk mendapatkan nomor pendaftaran.<br>
-        2.	Form pendaftaran ini bukan menjadi bukti siswa tersebut diterima di SMA Dharma Karya. Siswa dinyatakan diterima apabila telah menyelesaikan administrasi dan mendapatkan nomor pendaftaran.
-
+        1. Setelah administrasi selesai, serahkan form ini ke bagian pendaftaran untuk mendapatkan nomor pendaftaran.<br>
+        2. Form ini bukan bukti diterima, status diterima setelah pembayaran dan dapat nomor pendaftaran.
       <?php elseif ($status_pembayaran === 'Angsuran'): ?>
         <b>Catatan:</b><br>
-        Siswa telah melakukan pembayaran sebagian (angsuran).<br>
-        Simpan bukti ini sebagai tanda terima pembayaran.
+        Siswa telah melakukan pembayaran sebagian (angsuran). Simpan bukti ini.
       <?php elseif ($status_pembayaran === 'Lunas'): ?>
         <b>Catatan:</b><br>
-        Siswa telah menyelesaikan seluruh pembayaran.<br>
-        Simpan bukti ini sebagai tanda lunas dan konfirmasi pendaftaran.
+        Siswa telah menyelesaikan seluruh pembayaran. Simpan bukti ini sebagai tanda lunas.
       <?php else: ?>
         <b>Catatan:</b><br>
         Status pembayaran tidak diketahui.
       <?php endif; ?>
     </div>
-    <div class="footer-ttd-kanan">
-      <div class="ttd-block-kanan">
-        <div class="ttd-tanggal-kanan"><?= tanggal_id(date('Y-m-d')) ?></div>
-        <div class="ttd-petugas-kanan"><?= safe($petugas) ?></div>
-        <div class="ttd-label-kanan">(Petugas Pendaftaran)</div>
+
+    <!-- Tanda Tangan -->
+    <div class="footer-ttd">
+      <div class="footer-ttd-right">
+        <div class="ttd-tanggal"><?= tanggal_id(date('Y-m-d')) ?></div>
+        <div class="ttd-nama"><?= safe($petugas) ?></div>
+        <div class="ttd-label">(Petugas Pendaftaran)</div>
       </div>
     </div>
-  </div> 
+  </div>
 </body>
 </html>
+
