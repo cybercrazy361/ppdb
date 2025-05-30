691a5f5326857a05a9d25477199d557d549bfa2a
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-05-30 08:52:19 +0700

    update

--- pendaftaran/cek_koneksi.php
+++ pendaftaran/cek_koneksi.php
@@ -1,8 +1,12 @@
 <?php
-$result = @file_get_contents('https://google.com/');
-if ($result) {
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Tes PHP jalan!<br>";
+
+$hasil = @file_get_contents('https://google.com/');
+if ($hasil) {
     echo "Internet OK";
 } else {
     echo "Internet BERMASALAH dari server";
 }
-?>
