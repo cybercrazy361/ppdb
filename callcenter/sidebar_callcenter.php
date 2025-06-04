607f9da6f5bc17c578bcda8e10beebbfab58d21b
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-06-04 22:37:08 +0700

    update

--- callcenter/sidebar_callcenter.php
+++ callcenter/sidebar_callcenter.php
@@ -15,13 +15,13 @@
       <span>Dashboard</span>
     </a>
      <!-- LABEL CALON SISWA -->
-   <!--  <div class="sidebar-section-label">Progres siswa</div>
-   
-<a href="input_progres_pendaftaran" class="nav-link <?= ($active=='progressiswa'?'active':'') ?>">
-  <i class="fas fa-user-plus"></i>
-  <span>Input Daftar Calon Siswa</span>
-</a>
--->
+      <!--  <div class="sidebar-section-label">Progres siswa</div>
+      
+    <a href="input_progres_pendaftaran" class="nav-link <?= ($active=='progressiswa'?'active':'') ?>">
+      <i class="fas fa-user-plus"></i>
+      <span>Input Daftar Calon Siswa</span>
+    </a>
+    -->
 
     <!-- LABEL CALON SISWA -->
     <div class="sidebar-section-label">CALON SISWA</div>
