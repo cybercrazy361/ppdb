86afc27339fe074276f471520bf7994a23fa69c9
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-06-05 14:18:23 +0700

    update

--- pendaftaran/dashboard_pendaftaran.php
+++ pendaftaran/dashboard_pendaftaran.php
@@ -101,8 +101,8 @@ $conn->close();
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Dashboard <?=htmlspecialchars($unit)?> – SPMB</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-  <link rel="stylesheet" href="sidebar_pendaftaran_styles.css">
-  <link rel="stylesheet" href="pendaftaran_dashboard_styles.css">
+  <link rel="stylesheet" href="../assets/css/sidebar_pendaftaran_styles.css">
+  <link rel="stylesheet" href="../assets/css/pendaftaran_dashboard_styles.css">
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
