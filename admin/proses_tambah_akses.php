093e10122d0f7be26fdeba6df7c0ed20acea8993
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-06-17 07:51:18 +0700

    update

--- admin/proses_tambah_akses.php
+++ admin/proses_tambah_akses.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// ...lanjutan script kamu
+
 include '../database_connection.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
